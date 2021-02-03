3d231b7455ee244ba5a6340c3c9956169b8ee235
Ernestas-wq <ernestas.lakstinis1@gmail.com>
2021-02-03 19:03:43 +0200

    Style fixes, adding link to readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Movie Database Project
 
-A movie search app using the popular OMDb API. Try it out at...
+A movie search app using the popular OMDb API. Try it out at...https://moviedb-api-react.netlify.app/
--- src/pages/About.js
+++ src/pages/About.js
@@ -35,28 +35,23 @@ const About = () => {
             ipsa quod excepturi ex cum doloremque, ad fugit!Neque ratione temporibus vel, maxime
             tempore assumenda doloremque excepturi quis blanditiis, dolore doloribus reprehenderit
             perspiciatis amet corrupti exercitationem, itaque voluptate aliquam voluptatum inventore
-            ex. Fuga omnis ipsam culpa fugit quaerat? Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Perferendis doloremque officiis doloribus officia ipsum tempora
-            laudantium ad odit laborum voluptatem, similique obcaecati veniam maiores ipsam quae
-            aut, esse, dolor suscipit.Aut quis enim obcaecati doloribus natus quibusdam explicabo
-            magni. Laboriosam mollitia at laborum earum error minima sapiente, iste, illo dolorum
-            suscipit quae. Temporibus voluptatum pariatur quaerat dolorem adipisci harum fugiat!
           </p>
           <div className="about__bottom">
             <button className="btn">Contact Us</button>
             <div className="about__media">
-              <a href="https://github.com/Ernestas-wq/" target="_blank">
+              <a href="https://github.com/Ernestas-wq/" target="_blank" rel="noreferrer">
                 <AiFillGithub />
               </a>
-              <a href="https://www.youtube.com/" target="_blank">
+              <a href="https://www.youtube.com/" target="_blank" rel="noreferrer">
                 <AiFillYoutube />
               </a>
-              <a href="https://twitter.com/" target="_blank">
+              <a href="https://twitter.com/" target="_blank" rel="noreferrer">
                 <AiFillTwitterCircle />
               </a>
               <a
                 href="https://www.linkedin.com/in/ernestas-lak%C5%A1tinis-1334241bb?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BjITOZGWZS7CRCyYvqWq5BA%3D%3D"
                 target="_blank"
+                rel="noreferrer"
               >
                 <AiFillLinkedin />
               </a>
